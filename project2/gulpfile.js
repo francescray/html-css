1c85d2bd29417c37bdba2425fb054377c73a7ba2
francescray <277450201@qq.com>
2015-12-14 01:29:34 +0800

    最新修改自适应手机，宽长按照要求来

---
+++ project2/gulpfile.js
@@ -0,0 +1,23 @@
+var mGulp = require('gulp');
+ 
+// 引入gulp组件（插件）
+var mUglify = require('gulp-uglify');
+var mRename = require('gulp-rename');
+ 
+// 定义gulp任务
+mGulp.task('minjs', function() {
+	// 源地址
+	mGulp.src('./*.html')
+	// 压缩
+	.pipe(mUglify({
+		// 保留部分注释
+		preserveComments: 'some',
+	}))
+	// 重命名
+	.pipe(mRename({
+		// 文件后缀
+		extname: '.min.html',
+	}))
+	// 目标地址
+	.pipe(mGulp.dest('./'));
+});
